91f9e4d03806e9d9dc0c05edc34ec4efb7c07b33
jlcanela <jlcanelam@gmail.com>
2024-12-10 01:08:03 +0100

    fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import suidPlugin from "@suid/vite-plugin";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' 
+    ? '/news/' 
+    : '/',
   plugins: [suidPlugin(), solidPlugin()],
   build: {
     target: "esnext",
